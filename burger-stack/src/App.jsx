c30a207985daf3e200b2943d61053812194596e2
Tanner Hunt <thunt@dev-10.com>
2026-04-30 13:35:47 -0600

    fixed an error in the import statements

--- burger-stack/src/App.jsx
+++ burger-stack/src/App.jsx
@@ -3,11 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import './components/Page.jsx'
-import './components/Nav'
-import './components/BurgerView'
-import './components/IngredientPicker'
-import './components/Options'
+import Page from './components/Page.jsx'
+import Nav from './components/Nav.jsx'
+import BurgerView from './components/BurgerView.jsx'
+import IngredientPicker from './components/IngredientPicker.jsx'
+import Options from './components/Options.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
